9a93e9633456521df21331e07f75213ed2a7079a
hemant3203 <prajapatihero341@gmail.com>
2025-09-23 22:00:13 +0530

    UserAuth Hook

---
+++ frontend/expense-tracker/src/components/Cards/CharAvatar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CharAvatar = () => {
+  return 
+    
+};
+
+export default CharAvatar
--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -3,6 +3,7 @@ import { SIDE_MENU_DATA } from '../../utils/data'
 import {useContext} from 'react';
 import { UserContext } from '../../context/UserContext';
 import {useNavigate} from 'react-router-dom'
+import CharAvatar from '../Cards/CharAvatar';
 
 const SideMenu = ({activeMenu}) => {
 
@@ -26,12 +27,16 @@ const SideMenu = ({activeMenu}) => {
   };
   return <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/5 p-5 sticky top-[61px] z-20">
     <div className="flex flex-col items-center gap-3 mt-3 mb-7">
-      {user?.profileImageUrl ?(
+      {!user?.profileImageUrl ?(
       <img 
       src={user?.profileImageUrl||""}
       alt="Profile Image"
       className="w-20 h-20 bg-slate-400 rounded-full"
-      />):<></>}
+      />):(<CharAvatar
+         fulName={user?.fullName}
+        width="w-20"
+      height="h-20"
+    style="text-xl"/>)}
 
       <h5 className="text-gray-950 font-medium leading-6">
         {user?.fullName||""}
