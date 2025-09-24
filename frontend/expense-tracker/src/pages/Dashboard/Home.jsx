b5cba85c6a88f5352acb5fb67d1ba15723a0a987
hemant3203 <prajapatihero341@gmail.com>
2025-09-24 12:57:01 +0530

    Fincancial Overview and customPie charts

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -10,6 +10,7 @@ import{LuHandCoins,LuWalletMinimal} from 'react-icons/lu';
 import {IoMdCard} from 'react-icons/io';
 import { addThousandsSeparator } from '../../utils/helper';
 import RecentTransactions from '../../components/Dashboard/RecentTransactions';
+import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 
 
 const Home = () => {
@@ -51,7 +52,7 @@ useEffect(() => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <InfoCard
            icon={<IoMdCard/>}
            label="Total Balance"
@@ -75,13 +76,20 @@ useEffect(() => {
            />
 
 
-        </div>
+        </div> */}
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
           <RecentTransactions
           transactions={dashboardData?.recentTransaction}
           onSeeMore={()=> navigate("/expense")}
           />
+
+         <FinanceOverview
+          totalBalance={dashboardData?.totalBalance||0}
+          totalIncome={dashboardData?.totalIncome||0}
+          totalExpense={dashboardData?.totalExpense||0}
+          />
+
         </div>
         
         </div>
